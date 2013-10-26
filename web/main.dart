75ac024011c0865e1ab17ca5fe033774571a2e01
Vallet Loïc <eagleofdeath13@gmail.com>
2013-10-26 22:12:01 +0200

    Ajout du bouton page suivante Apparition du FeedBack et disparition du combobox Création d'une classe Utils pour la conversion de JSON (format texte) en objet Scène (à faire évoluer pour gérer la navigation d'arbre des possibles)

--- web/main.dart
+++ web/main.dart
@@ -2,36 +2,35 @@ import 'dart:html';
 import 'dart:convert';
 import 'scene.dart';
 import 'package:json_object/json_object.dart';
+import 'utils.dart';
 
+String currentNode = "";
+Scene currentScene;
 DivElement mainEl;
 void main() {
+  currentNode = "C1Q1";
+  window.localStorage.clear;
   mainEl = querySelector("#sample_container_id");
-  startNode("C1Q1");
-  ButtonElement b = querySelector("#button");
-  b.onClick.listen(onButton);
+  startNode(currentNode);
+  ButtonElement b = querySelector("#nextPageButton");
+  b.onClick.listen(onNextPage);
 }
 startNode(String nodeNumber){
   HttpRequest.getString("scenes/"+nodeNumber+'.json').then((response){
-    JsonObject jsonRoot = new JsonObject.fromJsonString(response);
-    List<String> choix = new List<String>();
-    List<String> valeur = new List<String>();
-    List<String> feedback = new List<String>();
-    print(jsonRoot.scene.possibilite.choix[0]);
-    choix.add(jsonRoot.scene.possibilite.choix[0]);
-    choix.add(jsonRoot.scene.possibilite.choix[1]);
-    choix.add(jsonRoot.scene.possibilite.choix[2]);
-    valeur.add(jsonRoot.scene.possibilite.valeur[0]);
-    valeur.add(jsonRoot.scene.possibilite.valeur[1]);
-    valeur.add(jsonRoot.scene.possibilite.valeur[2]);
-    feedback.add(jsonRoot.scene.possibilite.feedback[0]);
-    feedback.add(jsonRoot.scene.possibilite.feedback[1]);
-    feedback.add(jsonRoot.scene.possibilite.feedback[2]);
-    Scene s = new Scene(jsonRoot.scene.texte,choix,valeur,feedback);
-    writeSceneToBook(s);
+    currentScene = Utils.jsonObjectToScene(response);
+    writeSceneToBook(currentScene);
   });
 }
-onButton(MouseEvent e){
-
+onNextPage(MouseEvent e){
+  SelectElement se = querySelector("#conjugaison");
+  int selectedIndex = se.selectedIndex;
+  window.localStorage[currentNode] = se.item(selectedIndex).value;
+  DivElement de = new DivElement();
+  de.appendText(se.item(selectedIndex).text);
+  se.parent.insertBefore(de,se);
+  se.remove();
+  writeFeedbackToBook(currentScene.feedback.elementAt(selectedIndex));
+  print(window.localStorage[currentNode]);
 }
 
 writeSceneToBook(Scene scene){
@@ -39,13 +38,19 @@ writeSceneToBook(Scene scene){
   List<String> textParts = scene.text.split("*");
   sceneDiv.appendText(textParts.elementAt(0));
   SelectElement se = new SelectElement();
+  se.id = "conjugaison";
   scene.choix.forEach((String s){
     OptionElement oe = new OptionElement();
+    oe.value = scene.valeur.elementAt(scene.choix.indexOf(s));
     oe.text = s;
-    oe.value = s;
     se.append(oe);
   });
   sceneDiv.append(se);
   sceneDiv.appendText(textParts.elementAt(1));
   mainEl.append(sceneDiv);
+}
+writeFeedbackToBook(String feedback){
+  DivElement feedbackDiv = new DivElement();
+  feedbackDiv.appendText(feedback);
+  mainEl.append(feedbackDiv);
 }
---
+++ web/utils.dart
@@ -0,0 +1,23 @@
+import 'scene.dart';
+import 'package:json_object/json_object.dart';
+
+class Utils {
+  utils();
+  static Scene jsonObjectToScene(String json){
+    JsonObject jsonRoot = new JsonObject.fromJsonString(json);
+    List<String> choix = new List<String>();
+    List<String> valeur = new List<String>();
+    List<String> feedback = new List<String>();
+    print(jsonRoot.scene.possibilite.choix[0]);
+    choix.add(jsonRoot.scene.possibilite.choix[0]);
+    choix.add(jsonRoot.scene.possibilite.choix[1]);
+    choix.add(jsonRoot.scene.possibilite.choix[2]);
+    valeur.add(jsonRoot.scene.possibilite.valeur[0]);
+    valeur.add(jsonRoot.scene.possibilite.valeur[1]);
+    valeur.add(jsonRoot.scene.possibilite.valeur[2]);
+    feedback.add(jsonRoot.scene.possibilite.feedback[0]);
+    feedback.add(jsonRoot.scene.possibilite.feedback[1]);
+    feedback.add(jsonRoot.scene.possibilite.feedback[2]);
+    return new Scene(jsonRoot.scene.texte,choix,valeur,feedback);
+  }
+}
